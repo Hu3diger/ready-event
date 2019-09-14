cb38cdd15af090483b829ccebecd6ecade5db721
Hu3diger <martinruediger2010@gmail.com>
2019-09-13 21:55:33 -0300

    First steps

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  template: `
+  <p>This is the title {{ title }}</p>
+  <router-outlet></router-outlet>
+  <p>This is the footer of the page</p>
+  `
 })
 export class AppComponent {
   title = 'ready-event';
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,9 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './pages/about/about.component';
 
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: '',
+    component: AppComponent
+  },
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
