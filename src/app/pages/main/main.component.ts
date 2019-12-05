06861da419ef85b2a60da943e443d293ac511138
Mateus Rafael de Campos Martins <games.martinsfly@gmail.com>
2019-12-05 18:25:01 -0300

    layout defined for list teams screen MAIN

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,30 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { TeamService } from 'src/app/services/team.service';
 import { ToastrService } from 'ngx-toastr';
 import { Team } from 'src/app/model/Team';
 
 @Component({
-    selector: 'app-main-page',
-    templateUrl: './main.component.html'
+  selector: 'app-main-page',
+  templateUrl: './main.component.html',
+  styleUrls: ['./main.component.scss']
 })
-
-export class MainComponent {
+export class MainComponent implements OnInit {
 
   team: Team;
+
   constructor(
     private teamService: TeamService,
     private toastr: ToastrService
-  ){
+  ) {
     this.team = new Team();
   }
 
-  create(){
-    this.team.name = "Teste via angular";
-    this.team.slug = "teste via angular jamais sera via react";
-    this.team.website = "www.comercioslug.com";
-    this.team.institution = "bacana";
-    this.teamService.createTeam(this.team).then(result => {
-      console.log(result.data)
-    });
+  ngOnInit() {
+
+  }
+
+  create() {
+
   }
 }
